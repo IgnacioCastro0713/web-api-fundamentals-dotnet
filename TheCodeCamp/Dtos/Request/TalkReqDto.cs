7ce122fc1083c340887248a9b5df7dcfea1a0941
José Ignacio Menchaca Castro <31707457+IgnacioCastro0713@users.noreply.github.com>
2020-05-20 14:21:37 -0500

    Add files via upload

---
+++ TheCodeCamp/Dtos/Request/TalkReqDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheCodeCamp.Dtos.Request
+{
+    public class TalkReqDto
+    {
+        public int TalkId { get; set; }
+        public string Title { get; set; }
+        public string Abstract { get; set; }
+        public int Level { get; set; }
+    }
+}
